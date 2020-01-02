5595ad0385869ba908e6181943841445a51631f0
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-01-02 15:14:12 -0300

    Added method _get_url()

--- widepay/widepay.py
+++ widepay/widepay.py
@@ -5,4 +5,11 @@ from requests.auth import HTTPBasicAuth
 
 class WidePay(object):
 
-    pass
+    _URL = 'https://api.widepay.com/v1'
+
+    def __init__(self, id, token):
+        self.id = id
+        self.token = token
+
+    def _get_url(self, path):
+        return f'{self._URL}{path}'
