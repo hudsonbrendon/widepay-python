529f131baf36db09254e9ff04e5e6bf0362ec988
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-01-02 15:14:42 -0300

    Added method _requests()

--- widepay/widepay.py
+++ widepay/widepay.py
@@ -13,3 +13,17 @@ class WidePay(object):
 
     def _get_url(self, path):
         return f'{self._URL}{path}'
+
+    def _request(self, url, method, data=None):
+        if method.lower() == 'get':
+            request = requests.get(url, auth=self.authentication())
+        elif method.lower() == 'post':
+            request = requests.post(url, data=data, auth=self.authentication())
+        elif method.lower() == 'put':
+            request = requests.put(url, data=data, auth=self.authentication())
+        elif method.lower() == 'delete':
+            request = requests.delete(url, auth=self.authentication())
+        else:
+            pass
+        return request.json()
+
